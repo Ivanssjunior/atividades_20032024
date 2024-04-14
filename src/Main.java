870760b77ecf01d1517baf4c59c851ea9856fa3a
Ivan <ivanssouzajr@gmail.com>
2024-04-14 16:30:39 +0900

    Simple user interface to calculate salary and commission.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner newUser = new Scanner(System.in);
+
+
+        System.out.println("Digite seu nome");
+        String name = newUser.nextLine();
+
+        System.out.println("Ola " +name+ ", qual o seu salario?");
+        double salary = newUser.nextDouble();
+
+        System.out.println("Qual o valor bruto em vendas realizado?");
+        double sales = newUser.nextDouble();
+
+        System.out.println("Qual o valor de comissao por vendas?");
+
+        double commission = newUser.nextDouble();
+        double finalcommission = commission/100;
+        double totalcommission = sales * finalcommission;
+
+        double totalsalary = salary + totalcommission;
+
+        System.out.printf("%s, sua comissao foi de $%.2f, e seu salario total e $%.3f\n", name, totalcommission, totalsalary);
+
+        newUser.close();
+    }
+}
