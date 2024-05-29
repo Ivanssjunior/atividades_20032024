6b781c02a0c92af8f73631c59d3a67071ff154f2
Ivan <ivanssouzajr@gmail.com>
2024-05-29 14:54:27 +0900

    Basic intalment payment calculator.

--- src/BNPLCalculator.java
+++ src/BNPLCalculator.java
@@ -4,16 +4,27 @@ public class BNPLCalculator{
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the cost of your product");
-        double productCost = scanner.nextInt();
+        double productCost = scanner.nextDouble();
 
-        System.out.println("Please enter the ammount");
-        int productAmmount = scanner.nextInt();
+        System.out.println("Please enter the amount");
+        int productAmount = scanner.nextInt();
 
-        double finalPrice = productAmmount * productCost;
+        System.out.println("Enter the interest rate: ");
+        double interestRate = scanner.nextDouble();
 
-        System.out.println("Total Ammount is: " + productCost);
-        System.out.println("Total Products: " + productAmmount);
-        System.out.println("Final price: " + finalPrice);
+        double interestCalc = interestRate / 100;
+
+        double priceWithoutInterest = productAmount * productCost;
+
+        double finalPrice = priceWithoutInterest * interestCalc + priceWithoutInterest;
+
+        double interestCost = finalPrice - priceWithoutInterest;
+
+        System.out.println("Product Amount is: " + productCost);
+        System.out.println("Total Products: " + productAmount);
+        System.out.println("Price Without Interest: " + priceWithoutInterest);
+        System.out.println("Price With Interest: " + finalPrice);
+        System.out.println("Interest Cost: " + interestCost);
 
         scanner.close();
     }
