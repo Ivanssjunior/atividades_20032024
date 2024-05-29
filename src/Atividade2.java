9b145db4a760a37197ca05759885a43e430214d2
Ivan <ivanssouzajr@gmail.com>
2024-05-29 11:10:34 +0900

    Added on Atividade2 a IF/ELSE statement to choose if you want to switch the values or not.

--- src/Atividade2.java
+++ src/Atividade2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Atividade2 {
     public static void main(String[] args) {
         Scanner newValue = new Scanner(System.in);
+        Scanner respostaUsuario = new Scanner (System.in);
 
         System.out.println("Digite um valor para A");
         double firstValue = newValue.nextDouble();
@@ -9,16 +10,31 @@ public class Atividade2 {
         System.out.println("Digite um valor para B");
         double secondValue = newValue.nextDouble();
 
-        System.out.printf("Valor A original: $%s, Valor original B: $%.2f\n", firstValue, secondValue);
+        
 
-        double temp = firstValue;
-        firstValue = secondValue;
-        secondValue = temp;
+        System.out.println("Voce gostaria de inverter o resultado? Digite SIM ou NAO");
+        
+        String userInput = respostaUsuario.nextLine();
+        
+        if(userInput.equalsIgnoreCase("Sim")){
+            
+            double temp = firstValue;
+            firstValue = secondValue;
+            secondValue = temp;
 
-        System.out.print("Valores trocados");
-        System.out.printf("Valor A Alterado: %s, Valor B alterado: $%.2f\n", firstValue, secondValue);
-
+            System.out.print("Valores trocados");
+            System.out.printf("Valor A Alterado: %s, Valor B alterado: $%.2f\n", firstValue, secondValue);
+            
+        } else if (userInput.equalsIgnoreCase("Nao")){
+            
+            System.out.printf("Valor A original: $%s, Valor original B: $%.2f\n", firstValue, secondValue);
+            
+        } else {
+            System.out.println("Resposta Invalida, Digite SIM ou NAO");
+        }
+        
         newValue.close();
+        respostaUsuario.close();
 
     }
 
