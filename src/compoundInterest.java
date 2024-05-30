b5fa57bc9f0ab05ea83e50866128aa6e892b3cd8
Ivan <ivanssouzajr@gmail.com>
2024-05-30 10:09:49 +0900

    Basic compund interest calculator.

--- src/compoundInterest.java
+++ src/compoundInterest.java
@@ -12,10 +12,31 @@ public class compoundInterest {
         System.out.println("How many years it will be? ");
         int timeAmount = scanner.nextInt();
 
-        double interestFormula = Math.pow((1 + interestRate/100), timeAmount);
-        double totalAmount = startAmount * interestFormula;
+        scanner.nextLine();
 
-        System.out.printf("The total amount after %d years is: %.2f%n", timeAmount, totalAmount);
+        System.out.println("It is a depreciation calculus?");
+        String userInput = scanner.nextLine();
+
+
+        if(userInput.equalsIgnoreCase("Yes")){
+
+            double depreciationFormula = Math.pow((1-interestRate/100), timeAmount);
+            double depreciationTotal = startAmount * depreciationFormula;
+
+            System.out.printf("Depreciation value in %d years is: %.2f ", timeAmount, depreciationTotal);
+
+        } else if(userInput.equalsIgnoreCase("No")){
+
+            double compoundFormula = Math.pow((1 + interestRate/100), timeAmount);
+            double totalAmount = startAmount * compoundFormula;
+
+            System.out.printf("The total amount after %d years is: %.2f%n", timeAmount, totalAmount);
+
+        } else {
+            System.out.println("Invalid, please enter YES or NO");
+        }
+
+
 
 
     scanner.close();
